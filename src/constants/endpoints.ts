1ca8e394d29099a578ae83600f4124ef687c71a9
Tihomir Selak <tknox.dr@gmail.com>
2024-03-17 13:38:52 +0100

    RC-08: Update cdn paths

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,6 +1,6 @@
 export enum Endpoints {
-  WordsEng = "https://cdn.tihomir-selak.from.hr/api/words-details-eng.json",
-  WordsCro = "https://cdn.tihomir-selak.from.hr/api/words-details-cro.json",
-  DailyWordEng = "https://cdn.tihomir-selak.from.hr/api/random/word-details-eng.json",
-  DailyWordCro = "https://cdn.tihomir-selak.from.hr/api/random/word-details-cro.json",
+  WordsEng = "https://cdn.tihomir-selak.from.hr/assets/api/words-details-eng.json",
+  WordsCro = "https://cdn.tihomir-selak.from.hr/assets/api/words-details-cro.json",
+  DailyWordEng = "https://cdn.tihomir-selak.from.hr/daily/word-details-eng.json",
+  DailyWordCro = "https://cdn.tihomir-selak.from.hr/daily/word-details-cro.json",
 }
